ca9d66a5063bd416130115b107d5ce1123edf732
eartharoid <git@eartharoid.me>
2026-02-02 00:20:07 +0000

    non-blocking animation & transition

--- clock.c
+++ clock.c
@@ -4,6 +4,36 @@
 #include "hardware/i2c.h"
 #include "ht16k33.h"
 
+#define STDIN_BUF_SIZE 128
+char input_buffer[STDIN_BUF_SIZE];
+int buffer_pos = 0;
+
+// Returns true only when a full line is ready
+bool get_line_non_blocking()
+{
+    int c = getchar_timeout_us(0);
+
+    while (c != PICO_ERROR_TIMEOUT)
+    {
+        // If we see a newline, terminate the string and return true
+        if (c == '\n' || c == '\r')
+        {
+            input_buffer[buffer_pos] = '\0'; // Null terminator
+            buffer_pos = 0;                  // Reset for next time
+            return true;
+        }
+        // Otherwise, add the char to the buffer if there's room
+        else if (buffer_pos < STDIN_BUF_SIZE - 1)
+        {
+            input_buffer[buffer_pos++] = (char)c;
+        }
+
+        // Check for more chars immediately
+        c = getchar_timeout_us(0);
+    }
+    return false;
+}
+
 int main()
 {
     stdio_init_all();
@@ -14,14 +44,10 @@ int main()
     gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
     gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
 
-    printf("Hello Clock!\n");
-
     display_init();
     display_set_brightness(3); // 25%
 
-again:
-    display_clear();
-    const uint8_t frames[6][5] = {
+    const uint8_t frames_awaiting[6][5] = {
         {0x40, 0x00, 0x00, 0x00, 0x00},
         {0x00, 0x40, 0x00, 0x00, 0x00},
         {0x00, 0x00, 0x00, 0x40, 0x00},
@@ -29,53 +55,126 @@ again:
         {0x00, 0x00, 0x00, 0x40, 0x00},
         {0x00, 0x40, 0x00, 0x00, 0x00},
     };
-    for (int r = 0; r < 4; r++)
+
+    const uint8_t frames_transition[8][5] = {
+        {0x40, 0x40, 0x00, 0x40, 0x40},
+        {0x40, 0x40, 0x00, 0x40, 0x40},
+        {0x00, 0x00, 0x00, 0x00, 0x00},
+        {0x00, 0x00, 0x00, 0x00, 0x00},
+        {0x40, 0x40, 0x00, 0x40, 0x40},
+        {0x40, 0x40, 0x00, 0x40, 0x40},
+        {0x00, 0x00, 0x00, 0x00, 0x00},
+        {0x00, 0x00, 0x00, 0x00, 0x00},
+    };
+
+    int frame_index = 0;
+    absolute_time_t next_frame_time = get_absolute_time();
+    const uint32_t frame_delay_ms = 250;
+
+    typedef enum
     {
-        for (int f = 0; f < 6; f++)
+        STATE_AWAITING_CONFIG,
+        STATE_CLOSING_INITIAL,
+        STATE_TRANSITION,
+        STATE_READY
+    } system_state_t;
+    system_state_t current_state = STATE_AWAITING_CONFIG;
+
+    while (current_state != STATE_READY)
+    {
+        // 1. Check for a full configuration line
+        if (get_line_non_blocking())
         {
-            display_set_buffer(frames[f]);
-            display_write();
-            sleep_ms(250);
+            printf("Received config: %s\n", input_buffer);
+            if (strncmp(input_buffer, "time", 4) == 0)
+            {
+                // ! todo
+                current_state = STATE_CLOSING_INITIAL;
+            }
+        }
+
+        // 2. Run animation logic
+        if (time_reached(next_frame_time))
+        {
+
+            if (current_state == STATE_AWAITING_CONFIG || current_state == STATE_CLOSING_INITIAL)
+            {
+                display_set_buffer(frames_awaiting[frame_index]);
+                display_write();
+
+                // If we were closing and just hit the last frame, move to transition
+                if (current_state == STATE_CLOSING_INITIAL && frame_index == 5)
+                {
+                    current_state = STATE_TRANSITION;
+                    frame_index = 0; // Reset index for the new animation
+                }
+                else
+                {
+                    frame_index = (frame_index + 1) % 6;
+                }
+            }
+            else if (current_state == STATE_TRANSITION)
+            {
+                display_set_buffer(frames_transition[frame_index]);
+                display_write();
+
+                frame_index++;
+                if (frame_index >= 8)
+                {
+                    current_state = STATE_READY; // This will exit the while loop next iteration
+                }
+            }
+
+            next_frame_time = delayed_by_ms(get_absolute_time(), 250);
         }
     }
 
-    display_char(0, '-');
-    display_char(1, '-');
-    display_char(3, '-');
-    display_char(4, '-');
-    display_write();
-    display_set_blink(2);
-    sleep_ms(2000);
-    display_set_blink(0);
-
-    display_char(0, '1');
-    display_char(1, '2');
-    display_colon(true);
-    display_char(3, '3');
-    display_char(4, '4');
-    display_write();
-
-    sleep_ms(2000);
-
-    display_char(0, ' ');
-    display_char(1, ' ');
-    display_colon(false);
-    display_char(3, '4');
-    display_char_with_dot(4, '5');
-    display_write();
-
-    sleep_ms(2000);
-
-    goto again;
-
-    // while (1) {
-    //     for (int i = 0; i < 15; i++) {
-    //         display_set_brightness(i);
-    //         sleep_ms(50);
+    // again:
+    //     display_clear();
+    //     const uint8_t frames[6][5] = {
+    //         {0x40, 0x00, 0x00, 0x00, 0x00},
+    //         {0x00, 0x40, 0x00, 0x00, 0x00},
+    //         {0x00, 0x00, 0x00, 0x40, 0x00},
+    //         {0x00, 0x00, 0x00, 0x00, 0x40},
+    //         {0x00, 0x00, 0x00, 0x40, 0x00},
+    //         {0x00, 0x40, 0x00, 0x00, 0x00},
+    //     };
+    //     for (int r = 0; r < 4; r++)
+    //     {
+    //         for (int f = 0; f < 6; f++)
+    //         {
+    //             display_set_buffer(frames[f]);
+    //             display_write();
+    //             sleep_ms(250);
+    //         }
     //     }
-    //     for (int i = 14; i >= 0; i--) {
-    //         display_set_brightness(i);
-    //         sleep_ms(50);
-    //     }
-    // }
+
+    //     display_char(0, '-');
+    //     display_char(1, '-');
+    //     display_char(3, '-');
+    //     display_char(4, '-');
+    //     display_write();
+    //     display_set_blink(2);
+    //     sleep_ms(2000);
+    //     display_set_blink(0);
+
+    //     display_char(0, '1');
+    //     display_char(1, '2');
+    //     display_colon(true);
+    //     display_char(3, '3');
+    //     display_char(4, '4');
+    //     display_write();
+
+    //     sleep_ms(2000);
+
+    //     display_char(0, ' ');
+    //     display_char(1, ' ');
+    //     display_colon(false);
+    //     display_char(3, '4');
+    //     display_char_with_dot(4, '5');
+    //     display_write();
+
+    //     sleep_ms(2000);
+
+    //     goto again;
 }
